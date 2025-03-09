d02ac8a10a6eade51b672ec24feed3ec35bed76b
misterSagnikDhar <sagnikdhar03@gmail.com>
2025-03-09 23:46:37 +0530

    Set the Allowed Hosts to all(*), and Debug to False on settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,12 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-h=x_7@9zwwh+oum7fp_-7ew3&tai8^&%r^l&_e00k&!lvsaep!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 # Production Build
-# DEBUG = False
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
