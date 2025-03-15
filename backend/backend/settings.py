707a499388bc453b84ef5babe0f11ec28514ba1a
misterSagnikDhar <sagnikdhar03@gmail.com>
2025-03-16 00:27:32 +0530

    updated code

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -147,7 +147,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 #MongoDB Configuration:
 # MONGO_URI = "mongodb://localhost:27018"
-MONGO_DB_NAME = "Image_Search"
+# MONGO_DB_NAME = "Image_Search"
 
 
 
@@ -156,4 +156,4 @@ MONGO_DB_NAME = "Image_Search"
 load_dotenv()
 
 UNSPLASH_API_KEY = os.getenv('UNSPLASH_API_KEY')
-MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27018/')
+MONGO_URI = os.getenv('MONGO_URI')
